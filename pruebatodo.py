1647114b72d19eb7276f0adcb1cee4ab5c17e5b0
bsl7890 <benjamin.santander.lopez.2016@gmail.com>
2025-04-17 10:15:45 -0400

    6

---
+++ pruebatodo.py
@@ -0,0 +1,34 @@
+# print("hola\vmundo")  # tabulador vertical
+# print("hola\tmundo")  # tabulador horizontal
+# print("hola\nmundo")  # salto de línea
+# print("hola\bmundo")  # retroceso
+# print("hola\amundo")  # alerta (bell)
+# print("hola\fmundo")  # salto de página (form feed)
+# print("hola\vmundo")  # otra vez tabulador vertical
+import winsound
+
+# Hacer una melodía simple
+winsound.Beep(800, 300)  # Tono 1
+winsound.Beep(1000, 300)  # Tono 2
+winsound.Beep(1200, 300)  # Tono 3
+winsound.Beep(1500, 300)  # Tono 4
+winsound.Beep(1800, 300)  # Tono 5
+winsound.Beep(2000, 300)  # Tono 6
+winsound.Beep(2200, 300)  # Tono 7
+winsound.Beep(2500, 300)  # Tono 8
+winsound.Beep(2800, 300)  # Tono 9
+winsound.Beep(3000, 300)  # Tono 10
+winsound.Beep(3300, 300)  # Tono 11
+winsound.Beep(3600, 300)  # Tono 12
+winsound.Beep(4000, 300)  # Tono 13
+winsound.Beep(4400, 300)  # Tono 14
+winsound.Beep(4500, 300)  # Tono 15
+winsound.Beep(4800, 300)  # Tono 16
+winsound.Beep(5000, 300)  # Tono 17
+winsound.Beep(5300, 300)  # Tono 18
+winsound.Beep(5600, 300)  # Tono 19
+winsound.Beep(6000, 300)  # Tono 20
+
+
+
+
